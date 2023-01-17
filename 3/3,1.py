962f3b9444369d086df218cb2a8cc2bc3c0d366a
JakubPatrykAdamczyk <abukaj1@vp.pl>
2023-01-17 18:48:20 +0100

    a

--- 3/3,1.py
+++ 3/3,1.py
@@ -1,6 +1,7 @@
 from keras.datasets import imdb
 import numpy as np
 from keras import models,layers,optimizers,losses,metrics
+import matplotlib.pyplot as plt
 number_tent=10000
 
 (train_data,train_labels),(test_data,test_labels)=imdb.load_data(num_words=number_tent)
@@ -52,3 +53,41 @@ history=model.fit(partial_x_train,
 
 history_dict=history.history
 print(history_dict.keys())
+#wykres strat trenowania i walidacji
+acc=history.history['acc']
+val_acc=history.history['val_acc']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+epochs=range(1,len(acc)+1)
+
+plt.plot(epochs,loss,'bo',label='Strata trenowania')
+plt.plot(epochs,val_loss,'b',label='Strata walidacji')
+plt.title('Strata trenowania i walidacji')
+plt.xlabel('epoki')
+plt.ylabel('strata')
+plt.show()
+
+#wykres dokładności
+plt.clf()#clear
+acc_value=history_dict['acc']
+val_acc_values=history_dict['val_acc']
+plt.plot(epochs,acc,'bo',label='Dokladnosc trenowania')
+plt.plot(epochs,val_acc,'b',label='Dokladnosc walidacji')
+plt.title('Strata trenowania i walidacji')
+plt.xlabel('epoki')
+plt.ylabel('strata')
+plt.show()
+
+#ponowne trenowanie modelu
+model=models.Sequential()
+model.add(layers.Dense(16,activation='relu',input_shape=(number_tent,)))
+model.add(layers.Dense(16,activation='relu'))
+model.add(layers.Dense(1,activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+    loss='binary_crossentropy',
+    metrics=['accuracy'])
+model.fit(x_train,y_train,epochs=4,batch_size=512)
+results=model.evaluate(x_test,y_test)
+print(results)
+
