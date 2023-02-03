e5ffb19c7a53d94c0a9a39a38227491d2b253a74
JakubPatrykAdamczyk <abukaj1@vp.pl>
2023-02-03 18:52:43 +0100

    a

--- 5/5,2.py
+++ 5/5,2.py
@@ -206,7 +206,7 @@ train_datagen=ImageDataGenerator(rescale=1./255,
                                 zoom_range=0.2,
                                 horizontal_flip=True)
 
-test_datagem=ImageDataGenerator(rescale=1./255)
+test_datagen=ImageDataGenerator(rescale=1./255)
 
 
 train_generator=train_datagen.flow_from_directory(
